b82ecbdfcd277ca4579bb3be399ad899186fc5b0
NatyQA <natalia.quinteros@inacapmail.cl>
2024-11-27 19:14:08 -0300

    Agregar archivos de proyecto.

---
+++ MiPrimeraAplicacion/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MiPrimeraAplicacion
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MiPrimeraAplicacion/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MiPrimeraAplicacion
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
